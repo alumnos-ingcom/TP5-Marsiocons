51cb18bb0abc2a05a06042864c4910f4783591ee
marsiocons <maaarciosk8@gmail.com>
2021-05-22 21:46:15 -0300

    Ejercicio 1 terminado. Agregado archivos con funciones útiles

--- plantilla.py
+++ plantilla.py
@@ -1,16 +1,12 @@
 ################
-# Martín René - @martinvilu
+# Marcio Betanzo - @marsiocons
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
 
-# Reemplazar por las funciones del ejercicio
-
-
 def prueba():
-    """Toda la interacción con el usuario va acá"""
+    
     pass
 
 if __name__ == "__main__":
     prueba()
-
---
+++ tp5ej1.py
@@ -0,0 +1,23 @@
+################
+# Marcio Betanzo - @marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+from utilidades import ingreso_entero
+def es_par(numero):
+    mitad_numero = int(numero / 2)
+    if (mitad_numero * 2 == numero):
+        return True
+    else:
+        return False
+
+def prueba():
+    num = ingreso_entero("Ingrese un número para saber si es par o impar!")
+    if (es_par(num)):
+        print("Es par!")
+    else:
+        print("Es impar!")
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
