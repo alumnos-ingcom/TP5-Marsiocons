52bea3eaea6f42239c7db6e600710d3fdb07fd31
marsiocons <maaarciosk8@gmail.com>
2021-05-22 23:28:54 -0300

    Ejercicio terminado!

---
+++ tp5ej5.py
@@ -0,0 +1,24 @@
+################
+# Marcio Betanzo - @marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def inversion_may_min(texto):
+    new_texto = []
+    for i in texto:
+        if (i.isupper()):
+            new_texto.append(i.lower())
+        else:
+            new_texto.append(i.upper())
+        texto = "".join(new_texto)
+    return texto
+
+def prueba():
+    print("Ingrese el texto para invertir las mayúsculas y minúsculas:")
+    texto = str(input())
+    print(inversion_may_min(texto))
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
