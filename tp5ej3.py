0816c89fb31316ce4dc164956baa2ecc93e025db
marsiocons <maaarciosk8@gmail.com>
2021-05-22 22:24:23 -0300

    Ejercicio terminado!

---
+++ tp5ej3.py
@@ -0,0 +1,30 @@
+################
+# Marcio Betanzo - @marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+import utilidades as ing
+
+def tribonacci(numero_vez):
+    
+    if (numero_vez < 4):
+        raise ing.IngresoIncorrecto("Error. El número tiene que ser mayor a 3!")
+    
+    ant = 1
+    ant_ant = 1
+    ant_ant_ant = 1
+    for i in range(numero_vez):
+        if (i > 2):
+            resultado = ant + ant_ant + ant_ant_ant
+            ant_ant_ant = ant_ant
+            ant_ant = ant
+            ant = resultado
+    return resultado
+def prueba():
+    num = ing.ingreso_entero("Ingrese la n-esima vez de tribonacci: ")
+    print(tribonacci(num))
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
