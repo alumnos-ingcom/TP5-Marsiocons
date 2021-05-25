1425218018129509ca4b2789b15e4a244573578d
marsiocons <maaarciosk8@gmail.com>
2021-05-25 13:27:16 -0300

    Base del ejercicio 8

---
+++ tp5ej8.py
@@ -0,0 +1,19 @@
+################
+# Marcio Betanzo - @marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+## chr retorna el carácter que representa el número
+## ord retorna el número que representa el carácter
+
+def enrcriptar_cesar(texto, rotacion):
+    
+#def desencriptar_cesar(texto, rotacion):
+
+def prueba():
+    
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
