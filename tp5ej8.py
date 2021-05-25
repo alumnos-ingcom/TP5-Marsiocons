99c992722d53c2e365ab7214c2fc507e7fa23562
marsiocons <maaarciosk8@gmail.com>
2021-05-25 15:51:01 -0300

    sólo intercambia mayúsculas por mayúsculas, minúsculas por minúsculas y números por números. Los demás carácteres los deja como está

--- tp5ej8.py
+++ tp5ej8.py
@@ -17,21 +17,33 @@ def encriptar_cesar(texto, rotacion):
     Z = 90
     a = 97
     z = 122
+    num0 = 48
+    num9 = 57
     
     lista_nuevo_texto = []
     
     for i in texto:
         dato_unicode =  ord(i)
         if (dato_unicode != espacio):
-            # minúsuculas
+            # minúsuculas a - z
             if (a <= dato_unicode <= z):
                 if (dato_unicode + rotacion > z):
                     dato_unicode = dato_unicode - 26
-            # mayúsculas
-            if (A <= dato_unicode <= Z):
+                lista_nuevo_texto.append(chr(dato_unicode+rotacion))
+            # mayúsculas A - Z
+            elif(A <= dato_unicode <= Z):
                 if (dato_unicode + rotacion > Z):
                     dato_unicode = dato_unicode - 26
-            lista_nuevo_texto.append(chr(dato_unicode+rotacion))
+                lista_nuevo_texto.append(chr(dato_unicode+rotacion))
+            ## numeros 0 - 9
+            elif(num0 <= dato_unicode <= num9):
+                while rotacion > 9:
+                    rotacion = rotacion - 9
+                if (dato_unicode + rotacion > num9):
+                    dato_unicode = dato_unicode - 10
+                lista_nuevo_texto.append(chr(dato_unicode+rotacion))
+            else:
+                lista_nuevo_texto.append(str(i))
         else:
             lista_nuevo_texto.append(i)
     nuevo_texto = "".join(lista_nuevo_texto)
@@ -41,7 +53,7 @@ def encriptar_cesar(texto, rotacion):
 #def desencriptar_cesar(texto, rotacion):
 
 def prueba():
-    encriptar_cesar("R", 15)
+    encriptar_cesar("13ASD!!!...asd31", 15)
     pass
 
 if __name__ == "__main__":
