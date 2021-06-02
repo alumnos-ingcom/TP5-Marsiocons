80bfc227e81020b6f8e639a8e42df251ca31c583
marsiocons <maaarciosk8@gmail.com>
2021-06-02 20:30:49 -0300

    Cambios en el funcionamiento de encriptar_cesar. Menos código, más legible

--- tp5ej8.py
+++ tp5ej8.py
@@ -13,14 +13,10 @@ def encriptar_cesar(texto, rotacion):
     
     lista_nuevo_texto = []
     
-    carac_mayus_a = ord("A")
     carac_mayus_z = ord("Z")
-    carac_minus_a = ord("a")
     carac_minus_z = ord("z")
     carac_cero = ord("0")
     carac_nueve = ord("9")
-    
-    
     
     for i in texto:
         dato_unicode = ord(i)
@@ -29,85 +25,50 @@ def encriptar_cesar(texto, rotacion):
         if (es_letra_mayus(dato_unicode) or
             es_letra_minus(dato_unicode) or
             es_numero(dato_unicode)):
-        
-        
-            if (es_letra_mayus(dato_unicode) and
-                es_letra_minus(dato_encript)):
-                dato_encript = dato_encript + 6
+            
+            if (es_numero(dato_unicode)):
                 
-            if (es_numero(dato_unicode) and
-                es_letra_minus(dato_encript)):
-                dato_encript = dato_encript + 13
-            
-            if (es_letra_mayus(dato_unicode) and
-                dato_encript > carac_minus_z):
-                dato_encript = dato_encript + 6
-                distancia = obtener_distancia(dato_encript, carac_minus_z)
-                if (distancia > 10):
-                    distancia = distancia + 7
-                dato_encript = carac_cero + distancia - 1
-            
-            if (es_letra_minus(dato_unicode) and
-                dato_encript > carac_minus_z):
-                distancia = obtener_distancia(dato_encript, carac_minus_z)
-                if (distancia > 26):
-                    distancia = distancia + 6
-                if (distancia > 10):
-                    distancia = distancia + 7
-                dato_encript = carac_cero + distancia -1
-            
-            if (dato_encript > carac_minus_z):
-                dato_encript = dato_encript - 75
-            
-            if (es_numero(dato_unicode) and
-                es_letra_mayus(dato_encript)):
-                dato_encript = dato_encript + 7
-                   
-            if (es_letra_minus(dato_unicode) and
-                es_letra_mayus(dato_encript)):
-                dato_encript = dato_encript + 7
+                if (dato_encript > carac_nueve):
+                    dato_encript = dato_encript + 7
+                if (dato_encript > carac_mayus_z):
+                    dato_encript = dato_encript + 6
+                if (dato_encript > carac_minus_z):
+                    dist = obtener_distancia(dato_encript, carac_minus_z)
+                    dato_encript = carac_cero - 1 + dist
+                    
+            if (es_letra_mayus(dato_unicode)):
                 
+                if (dato_encript > carac_mayus_z):
+                    dato_encript = dato_encript + 6
+                if (dato_encript > carac_minus_z):
+                    dist = obtener_distancia(dato_encript, carac_minus_z)
+                    dato_encript = carac_cero - 1 + dist
+                    if (dato_encript > carac_nueve):
+                        dato_encript = dato_encript + 7
+                        
+            if (es_letra_minus(dato_unicode)):
                 
-            if (es_letra_mayus(dato_unicode) and
-                es_letra_mayus(dato_encript) and
-                rotacion > 25 and rotacion < 51):
-                dato_encript = dato_encript + 13
-            
-            if (es_letra_minus(dato_unicode) and
-                es_letra_minus(dato_encript) and
-                rotacion > 25 and rotacion < 51):
-                dato_encript = dato_encript + 13
-
-            
-            if (dato_encript > carac_mayus_z and
-                dato_encript < carac_minus_a):
-                dato_encript = dato_encript + 6
-            if (dato_encript > carac_nueve and
-                dato_encript < carac_mayus_a):
-                dato_encript = dato_encript + 7
-                
-
-            if (carac_cero <= dato_encript and
-                dato_encript <= carac_nueve):
-                lista_nuevo_texto.append(chr(dato_encript))
-            if (carac_mayus_a <= dato_encript and
-                dato_encript <= carac_mayus_z):
-                lista_nuevo_texto.append(chr(dato_encript))
-            if (carac_minus_a <= dato_encript and
-                dato_encript <= carac_minus_z):
-                lista_nuevo_texto.append(chr(dato_encript))
+                if (dato_encript > carac_minus_z):
+                    dist = obtener_distancia(dato_encript, carac_minus_z)
+                    dato_encript = carac_cero - 1 + dist
+                    if (dato_encript > carac_nueve):
+                        dato_encript = dato_encript + 7
+                    if (dato_encript > carac_mayus_z):
+                        dato_encript = dato_encript + 6
+                    
+            lista_nuevo_texto.append(chr(dato_encript))
         else:
             lista_nuevo_texto.append(chr(dato_unicode))
                 
     nuevo_texto = "".join(lista_nuevo_texto)
     return nuevo_texto
-            
+    
     
 def prueba():
-#     for i in range(50):
-#         encr = encriptar_cesar("0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz", i+1)
-#         print(f"{i+1} >: {encr}")
-    print(encriptar_cesar("z", 62))
+    
+    cesar = encriptar_cesar("a", 35)
+    print(cesar)
+    
     pass
 
 if __name__ == "__main__":
