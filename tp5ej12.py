47bfa063bba67968b72cf5ce2503da3aac9df93e
marsiocons <maaarciosk8@gmail.com>
2021-06-06 11:27:39 -0300

    primera version semi-funcional

--- tp5ej12.py
+++ tp5ej12.py
@@ -3,11 +3,33 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+from utilidades import lista_aleatoria
+
 def comparar_listas(lista_uno, lista_dos):
     
+    nueva_lista = list()
+    
+    for i in lista_uno:
+        for j in lista_dos:
+            if (j == i):
+                nueva_lista.append(i)
+                nueva_lista.append(j)
+    
+    cant_nueva_lista = len(nueva_lista)
+    cant_lista_uno_dos = len(lista_uno) + len(lista_dos)
+    
+    if (cant_nueva_lista == cant_lista_uno_dos):
+        return True
+    else:
+        return False
     pass
 
 def prueba():
+    
+    lista_uno = [10,20,33,43,12,2,2,2]
+    lista_dos = [10,20,33,43,12,1,2,1]
+    
+    print(comparar_listas(lista_uno, lista_dos))
     
     pass
 
