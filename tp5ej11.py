1803f72b8d952139e1d0137bb938869cb1ce242b
marsiocons <maaarciosk8@gmail.com>
2021-06-06 12:18:23 -0300

    Cambios menores

--- tp5ej11.py
+++ tp5ej11.py
@@ -9,7 +9,7 @@ def promedio_movil(lista_numeros, cant_valores = 3):
     
     lista_promedios = list()
     
-    for i in range(len(lista_numeros)-2):
+    for i in range(len(lista_numeros)-cant_valores-1):
         nueva_lista = lista_numeros[i:cant_valores+i]
         nuevo_promedio = promedio(nueva_lista)
         lista_promedios.append(nuevo_promedio)
@@ -18,20 +18,20 @@ def promedio_movil(lista_numeros, cant_valores = 3):
 
 def promedio(lista_numeros):
     
-    promedio = 0
+    promedio_op = 0
     
     for i in lista_numeros:
-        promedio = promedio + i
+        promedio_op = promedio_op + i
     
-    promedio = promedio / len(lista_numeros)
-    promedio = format(promedio, ".2f")
-    return float(promedio)
+    promedio_op = promedio_op / len(lista_numeros)
+    promedio_op = format(promedio_op, ".2f")
+    return float(promedio_op)
 
 def prueba():
     
     lista = lista_aleatoria()
     
-    print(promedio_movil(lista))
+    print(promedio_movil(lista, 3))
     
     pass
 
