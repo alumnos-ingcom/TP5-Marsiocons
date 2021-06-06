d8c686296c3c92439726bd8907c114447a16169f
marsiocons <maaarciosk8@gmail.com>
2021-06-06 12:10:41 -0300

    Ejercicio terminado!

--- tp5ej11.py
+++ tp5ej11.py
@@ -5,7 +5,18 @@
 
 from utilidades import lista_aleatoria
 
-def promedio_movil(lista_numeros):
+def promedio_movil(lista_numeros, cant_valores = 3):
+    
+    lista_promedios = list()
+    
+    for i in range(len(lista_numeros)-2):
+        nueva_lista = lista_numeros[i:cant_valores+i]
+        nuevo_promedio = promedio(nueva_lista)
+        lista_promedios.append(nuevo_promedio)
+    return lista_promedios
+    pass
+
+def promedio(lista_numeros):
     
     promedio = 0
     
@@ -13,17 +24,14 @@ def promedio_movil(lista_numeros):
         promedio = promedio + i
     
     promedio = promedio / len(lista_numeros)
-    return promedio
-    pass
+    promedio = format(promedio, ".2f")
+    return float(promedio)
 
 def prueba():
     
     lista = lista_aleatoria()
     
-    print(lista)
-    
     print(promedio_movil(lista))
-    
     
     pass
 
