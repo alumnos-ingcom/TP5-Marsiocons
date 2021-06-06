8e9c36b000082feebe53fdd5dc5fd8e32c4476ce
marsiocons <maaarciosk8@gmail.com>
2021-06-06 20:37:45 -0300

    commit inicial

---
+++ tp5ej13.py
@@ -0,0 +1,17 @@
+################
+# Marcio Betanzo - @marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def busqueda_palabra(texto, palabra):
+    
+    pass
+
+
+def prueba():
+    
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
