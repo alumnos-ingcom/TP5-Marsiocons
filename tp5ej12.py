72b1701ec1bad7d46a0d8dd0942c5525c629e118
marsiocons <maaarciosk8@gmail.com>
2021-06-06 11:16:20 -0300

    commit inicial

---
+++ tp5ej12.py
@@ -0,0 +1,16 @@
+################
+# Marcio Betanzo - @marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def comparar_listas(lista_uno, lista_dos):
+    
+    pass
+
+def prueba():
+    
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
