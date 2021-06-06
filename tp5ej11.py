9dbac5ede6d2c2f4d6c0f5b2f395862b30ea2b2a
marsiocons <maaarciosk8@gmail.com>
2021-06-06 11:01:12 -0300

    commit inicial

---
+++ tp5ej11.py
@@ -0,0 +1,16 @@
+################
+# Marcio Betanzo - @marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def promedio_movil(lista_numeros):
+    
+    pass
+
+def prueba():
+    
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
