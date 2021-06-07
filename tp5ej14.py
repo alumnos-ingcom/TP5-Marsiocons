d6b2e6bab37e4b0e072f6121d35ef9abd63fe7da
marsiocons <maaarciosk8@gmail.com>
2021-06-06 21:53:42 -0300

    Ejercicio terminado!

---
+++ tp5ej14.py
@@ -0,0 +1,35 @@
+################
+# Marcio Betanzo - @marsiocons
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+from utilidades import set_tiempo, get_tiempo, es_palindromo, ingreso_entero, IngresoIncorrecto
+
+def es_capicua(numero):
+    
+    numeros = str(numero)
+    
+    return es_palindromo(numeros)
+    
+    pass
+    
+
+
+def prueba():
+    
+    numero = ingreso_entero("""Ingrese un número mayor a 0
+    para determinar si es o no capicúa:""")
+    
+    if (numero <= 0):
+        raise IngresoIncorrecto("El número tiene que ser mayor a 0!")
+    else:
+        if (es_capicua(numero)):
+            print(f"El número ( {numero} ) es capicúa!")
+        else:
+            print(f"El número ( {numero} ) no es capícua!")
+    
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
--- utilidades.py
+++ utilidades.py
@@ -145,4 +145,36 @@ def remover_repetidos(lista):
             
     return lista_no_rep, lista_rep
     
+def es_palindromo(texto):
     
+    mitad_uno_texto = []
+    mitad_dos_texto = []
+    
+    ### Si es par, le sacamos letra del medio a la palabra
+    if ((len(texto) % 2) > 0):
+        
+        texto_par = []
+        
+        letra_medio = int(len(texto) / 2)
+        
+        for i in range(len(texto)):
+            if(texto[i] != texto[letra_medio]):
+                texto_par.append(texto[i])
+        texto = texto_par
+        
+    ### Obtenemos la primera mitad de letras de la palabra    
+    for i in range(int(len(texto)/2)):
+        mitad_uno_texto.append(texto[i])
+        
+    ### Obtenemos la segunda mitad de letras de la palabra
+    for i in range(int(len(texto)/2)):
+        if i == 0:
+            mitad_dos_texto.append(texto[i-1])
+        else:
+            mitad_dos_texto.append(texto[i*-1-1])
+            
+    ### Comparamos las mitades
+    if (mitad_uno_texto == mitad_dos_texto):
+        return True
+    else:
+        return False
